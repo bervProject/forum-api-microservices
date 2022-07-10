4becb79037436cb8a49fc8df82a1fbed2add57b5
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-07-10 08:07:38 +0700

    update paginated

--- app/thread-service/ThreadService/Model/Paginated.cs
+++ app/thread-service/ThreadService/Model/Paginated.cs
@@ -1,6 +1,6 @@
 namespace ThreadService.Model;
 
-public record struct Paginated
+public record struct Paginated()
 {
     public int Page { get; init; } = default!;
     public int Limit { get; init; } = default!;
